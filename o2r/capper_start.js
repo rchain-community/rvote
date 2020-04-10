9065e4c6c964a08340a0e6c975a3126cc96edd1c
Dan Connolly <dckc@madmode.com>
2020-04-10 00:37:06 -0500

    capper_start: define __dirname from import.meta.url

--- o2r/capper_start.js
+++ o2r/capper_start.js
@@ -3,6 +3,13 @@
  * ISSUE: this belongs in the capper library.
  */
 // @flow strict
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // $FlowFixMe
 import Capper from 'Capper';
 
