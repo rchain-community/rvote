c0fae13bf1d5667bad77e892ad8fbce5f26ecf8f
Dan Connolly <dckc@madmode.com>
2020-06-13 19:15:20 -0500

    rchat: turn DB operations into Rholang
      - factor out zulip_db_config
      - filter zerver_useractivity etc.
      - use NEW to avoid collisoin with js new

--- rchat/chain_replica.js
+++ rchat/chain_replica.js
@@ -1,50 +1,63 @@
 import postgres from 'postgres';  // ISSUE: AMBIENT
 
-async function main({ postgres }) {
-  
-  const sql = postgres({
+const zulip_db_config = {
     host: 'localhost',
     port: 5432,
     database: 'zulip',
     username: 'zulip',
     password: 'REPLACE_WITH_SECURE_POSTGRES_PASSWORD',
-  });
- 
-  // console.log(await messages(sql));
+};
 
-  const proc = 'proc1';
-  const channel = 'chan1';
+const zulip_ephemera = [
+  'django_session',
+  'fts_update_log',
+  'zerver_useractivity', 'zerver_useractivityinterval', 'zerver_userpresence',
+];
+
+
+async function main({ postgres }) {
+
+  const sql = postgres(zulip_db_config);
+
+  const proc = 'notify_mirror';
+  const channel = 'mirror';
   await create_notify_function(sql, proc, channel);
-  const names = await tables(sql);
-  console.log(names);
-  await Promise.all(names.map(async (tab_name) => {
+  const tables_of_record = (await pg_tables(sql))
+        .filter(({ table_schema, table_name }) => !zulip_ephemera.includes(table_name))
+        .map(({ table_schema, table_name }) => `${ table_schema }.${table_name}`);
+
+  await Promise.all(tables_of_record.map(async (tab_name) => {
     await add_notify_trigger(sql, tab_name, proc, channel);
   }));
 
   await sql.listen(channel, (payload) => {
-    const json = JSON.parse(payload);
-    console.log(channel, json);
+    const notice = JSON.parse(payload);
+    console.log({ op: notice.op, table_name: notice.table_name });
+    const rho = notice_as_rho(notice);
+    console.log(rho);
+    // TODO: deploy rho to chain
   });
 }
 
-
-async function messages(sql) {
-  return sql`
-    select * from zerver_message
+function notice_as_rho({ op, table_name, OLD = undefined, NEW = undefined}) {
+  const lit = val => val ? JSON.stringify(val) : 'Nil';
+  return `new deployId(\`rho:rchain:deployId\`), deployerId(\`rho:rchain:deployerId\`) in {
+    @{[*deployerId, "iddb_apply"]}!(${lit(op)}, ${lit(table_name)}, ${lit(OLD)}, ${lit(NEW)}, *deployId)
+  }
   `;
 }
 
-async function tables(sql) {
-  const result = await sql`
+
+async function pg_tables(sql) {
+  return await sql`
     SELECT
-        quote_ident(table_schema) || '.' || quote_ident(table_name) as tab_name
+        quote_ident(table_schema) as table_schema, quote_ident(table_name) as table_name
     FROM
         information_schema.tables
     WHERE
         table_schema NOT IN ('pg_catalog', 'information_schema')
         AND table_schema NOT LIKE 'pg_toast%'
 `;
-  return result.map(({ tab_name }) => tab_name);
 }
 
 async function create_notify_function(sql, proc, channel) {
@@ -62,8 +75,8 @@ begin
                       ', "table_name": ' || to_json(tg_table_name) ||
                       ', "relid": ' || to_json(tg_relid) ||
                       ', "op": ' || to_json(tg_op) ||
-                      case when tg_op = 'UPDATE' then ', "old": ' || row_to_json(OLD)::text else '' end ||
-                      case when tg_op in ('INSERT', 'UPDATE') then ', "new": ' || row_to_json(NEW)::text else '' end ||
+                      case when tg_op in ('UPDATE', 'DELETE') then ', "OLD": ' || row_to_json(OLD)::text else '' end ||
+                      case when tg_op in ('INSERT', 'UPDATE') then ', "NEW": ' || row_to_json(NEW)::text else '' end ||
                       '}')
   );
   RETURN NULL;
