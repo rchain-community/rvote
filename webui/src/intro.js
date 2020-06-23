f2d43e45f187e8a40cae5187668f0e748901088e
Dan Connolly <dckc@madmode.com>
2020-06-22 21:41:55 -0500

    QR code (e.g. for keys)

--- webui/src/intro.js
+++ webui/src/intro.js
@@ -1,10 +1,11 @@
 /**
  * intro - introductions, keys, registry URIs, avatars, icons, etc.
  */
+// @flow
 
 import makeBlockie from 'ethereum-blockies-base64';
 
-export function ui({ getElementById, inputElement }) {
+export function ui({ getElementById, inputElement, QRCode }) {
   // don't submit
   getElementById('intro1').addEventListener('submit', (event) => {
     event.preventDefault();
@@ -16,5 +17,13 @@ export function ui({ getElementById, inputElement }) {
     console.log('change!' + nickField.value);
     const png = makeBlockie(nickField.value);
     blockie1.setAttribute('src', png);
+  });
+
+  const key2Field = inputElement('key2');
+  const qr2 = getElementById('qr2');
+  key2Field.addEventListener('change', async (event) => {
+    console.log('key change! ' + key2Field.value);
+    const imgData = await QRCode.toDataURL(key2Field.value);
+    qr2.setAttribute('src', imgData);
   });
 }
