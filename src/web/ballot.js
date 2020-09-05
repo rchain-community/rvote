d4c3bf352af6c6f87fe079d30c75c18446949cbf
Dan Connolly <dckc@madmode.com>
2020-09-05 18:13:52 -0500

    get agenda questions from the blockchain

--- src/web/ballot.js
+++ src/web/ballot.js
@@ -1,91 +1,81 @@
 // @ts-check
 import { getAddrFromEth } from '@tgrospic/rnode-grpc-js';
 import { ethereumAddress } from '../eth/eth-wrapper';
+import { makeRNodeWeb } from '../rnode-web';
+import { makeRNodeActions } from './rnode-actions';
+import { testNet, getNodeUrls } from '../rchain-networks';
 
 const { keys, entries, fromEntries } = Object;
 
-/**
- * @param {unknown} proc
- */
-function rho2js(proc) {
-    function notNull(x, context) {
-        if (!x) {
-            throw new Error(`null/undefined ${context}`);
-        }
-        return x;
+function notNull(x, context) {
+    if (!x) {
+        throw new Error(`null/undefined ${context}`);
     }
-    function theObj(x, context) {
-        if (typeof x !== 'object') {
-            throw new Error(`${context} must be js object; got ${typeof x}`);
-        }
-        return x;
-    }
-    function theArray(x, context) {
-        if (!Array.isArray(x)) {
-            throw new Error(`${context} must be js Array; got ${typeof x}`);
-        }
-        return x;
-    }
-    const procObj = theObj(notNull(proc));
-    if ('ExprMap' in procObj) {
-        const data = theObj(notNull(proc['ExprMap']['data'], 'ExprMap'), 'ExprMap');
-        return fromEntries(entries(data).map(([key, valProc]) => [key, rho2js(valProc)]));
-    } else if ('ExprList' in procObj) {
-        const data = theArray(notNull(proc['ExprList']['data'], 'ExprList'), 'ExprList');
-        return data.map(rho2js);
-    } else if ('ExprString' in procObj) {
-        const data = procObj['ExprString']['data'];
-        if (typeof data !== 'string') {
-            throw new Error(`ExprString must be js String; got ${typeof data}`);
-        }
-        return data;
-    } else {
-        throw new Error(`not implemented: rho2js({${JSON.stringify(keys(proc))}})`)
-    }
+    return x;
 }
 
-// added to testnet Sep 5
-const agendaAddr = 'rho:id:g9hj5p66muy1n9d6bors494ikupqma4qj9q8kmxdgzhzkzz3957hmf';
-
-const agenda1 = {
-    "ExprMap": {
-        "data": {
-            "Daffy for Board Member": {
-                "ExprList": {
-                    "data": [{"ExprString": {"data": "no"}}, {"ExprString": {"data": "yes"}}]
-                }
-            },
-            "Donald for Board Member": {
-                "ExprList": {
-                    "data": [{"ExprString": {"data": "no"}}, {"ExprString": {"data": "yes"}}]
-                }
-            }
-        }
+function lookupExpr(uri) {
+    return `new return(\`rho:rchain:deployId\`), lookup(\`rho:registry:lookup\`)
+     in {
+    new valueCh in {
+      lookup!(\`${uri}\`, *valueCh) |
+      for (@value <- valueCh) {
+        return!(("Value from registry", value))
+      }
     }
-};
+  }`
+}
+
+/**
+ * @param {HTMLElement} elt
+ * @returns {HTMLButtonElement}
+ */
+function theButton(elt) {
+    if (!(elt instanceof HTMLButtonElement)) {
+        throw new Error('not Button');
+    }
+    return elt;
+}
+
+/**
+ * @param {HTMLElement} elt
+ * @returns {HTMLInputElement}
+ */
+function theInput(elt) {
+    console.log({ elt, ok: elt instanceof HTMLInputElement });
+    if (!(elt instanceof HTMLInputElement)) {
+        throw new Error('not input');
+    }
+    return elt;
+}
 
 /**
  * @param {{
  *  getElementById: typeof document.getElementById,
- *  getButtonById: typeof document.getElementById,
  *  createElement: typeof document.createElement,
+ *  fetch: typeof window.fetch,
  *  ethereumAddress: () => Promise<String>,
  *  }} powers
  */
-async function buildUI({ ethereumAddress, getElementById, getButtonById, createElement }) {
-    getElementById('ballotForm').addEventListener('submit', e => {
+function buildUI({ ethereumAddress, getElementById, createElement, fetch }) {
+    notNull(getElementById('ballotForm'), '#ballotForm').addEventListener('submit', e => {
         e.preventDefault();
     });
 
+
     const pickButton = getElementById('pickAccount');
     const addrField = getElementById('REVAddress');
+    let account;
     pickButton.addEventListener('click', async ev => {
-        const eth = await ethereumAddress();
-        const addr = getAddrFromEth(eth);
-        addrField.textContent = addr;
+        const ethAddr = await ethereumAddress();
+        const revAddr = getAddrFromEth(ethAddr);
+        addrField.textContent = revAddr;
+        account = { name: `gov ${revAddr.slice(0, 8) }`, ethAddr: ethAddr.replace(/^0x/, ''), revAddr };
     });
 
+    const rnodeWeb = makeRNodeWeb({ fetch });
     const questionListElt = getElementById('questionList');
+    const deployStatusElt = getElementById('deployStatus');
     const choiceElt = (name, label) => {
         const control = createElement('input');
         control.setAttribute('type', 'radio');
@@ -95,23 +85,61 @@ async function buildUI({ ethereumAddress, getElementById, getButtonById, createE
         elt.append(label);
         return elt;
     };
-    getButtonById('getQuestions').addEventListener('click', async ev => {
-        console.log('@@look up agenda on chain');
-        const qas = rho2js(agenda1);
-        entries(qas).forEach(([q, as], ix) => {
-            const qElt = createElement('li');
-            qElt.textContent = q;
-            as.forEach(ans => qElt.appendChild(choiceElt(`q${ix}`, ans)));
-            questionListElt.appendChild(qElt);
+    theButton(getElementById('getQuestions')).addEventListener('click', ev => {
+        if (!account) {
+            alert('choose account first');
+            return;
+        }
+        questionListElt.innerHTML = '';
+        deployStatusElt.innerHTML = '';
+
+        const agendaURI = theInput(getElementById('agendaURI')).value;
+        const phloLimit = parseInt(theInput(getElementById('phloLimit')).value);
+        console.log('looking up agenda on chain...');
+        console.log(testNet); //@@@
+        const node = getNodeUrls(testNet.hosts[0]); // TODO: get next validator?
+        const setStatus = status => {
+            deployStatusElt.textContent = status;
+        }
+        const code = lookupExpr(agendaURI);
+
+        // appSendDeploy has a strange API: only sends the returned data to the log.
+        // at least the log is handled with ocap discipline so we can interpose what we need!
+        let deployReturnData;
+        const { appSendDeploy } = makeRNodeActions(rnodeWeb, {
+            log(label, info, ...rest) {
+                if (label === 'DEPLOY RETURN DATA') {
+                    deployReturnData = info;
+                }
+                console.log(label, info, ...rest);
+            },
+            warn: console.warn,
         });
 
+        appSendDeploy({ node, code, account, phloLimit, setStatus })
+        .then(result => {
+            deployStatusElt.textContent = result;
+            const { args, cost } = deployReturnData;
+            const qas = args[1];
+            entries(qas).forEach(([q, as], ix) => {
+                const qElt = createElement('li');
+                qElt.textContent = q;
+                as.forEach(ans => qElt.appendChild(choiceElt(`q${ix}`, ans)));
+                questionListElt.appendChild(qElt);
+            });
+            })
+        .catch(err => {
+            console.log({ err });
+            deployStatusElt.textContent = `${err}`;
+        });
     });
 }
 
-
-buildUI({
-    ethereumAddress,
-    createElement: tag => document.createElement(tag),
-    getElementById: id => document.getElementById(id),
-    getButtonById: id => document.getElementById(id),
+window.addEventListener('DOMContentLoaded', (event) => {
+    buildUI({
+        ethereumAddress,
+        fetch: window.fetch,
+        createElement: tag => document.createElement(tag),
+        getElementById: id => document.getElementById(id),
+    });
 });
