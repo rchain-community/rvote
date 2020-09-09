97a5c3f122a7a3e24e4910c0c3d36c8ae45f6903
Dan Connolly <dckc@madmode.com>
2020-09-09 00:35:32 -0500

    copy code from rnode-client-js
      - import only default from cjs modules

---
+++ src/rho/check-balance.js
@@ -0,0 +1,16 @@
+// Rholang code to check REVs balance
+// - intended for use with RNode exploratory deploy
+export const checkBalance_rho = addr => `
+  new return, rl(\`rho:registry:lookup\`), RevVaultCh, vaultCh in {
+    rl!(\`rho:rchain:revVault\`, *RevVaultCh) |
+    for (@(_, RevVault) <- RevVaultCh) {
+      @RevVault!("findOrCreate", "${addr}", *vaultCh) |
+      for (@maybeVault <- vaultCh) {
+        match maybeVault {
+          (true, vault) => @vault!("balance", *return)
+          (false, err)  => return!(err)
+        }
+      }
+    }
+  }
+`
