eec60c05bdd43174159b50b93d9797cbd141d7cb
Dan Connolly <dckc@madmode.com>
2020-09-10 17:57:04 -0500

    tally: fix whichCurl; add docstrings / types; clean-up
      - access ambient authority only when invoked as script
        - todo: export tally, main functions
      - factor out intersection; use concise, local form of uniq
      - add jsdoc in places that seem useful
      - prune shell comment delimiters from module docstring

--- src/web/tally.js
+++ src/web/tally.js
@@ -1,8 +1,8 @@
 // usage: ./tally.sh [ballotfile] [transaction-server:port]
-// # https://github.com/rchain-community/rv2020/issues/35
-// # an account is counted only once for a choice.
-// # The case of a person voting for multiple choices the most recent is used.
-// # the check for the account being allowed to vote is not handled.
+// https://github.com/rchain-community/rv2020/issues/35
+// an account is counted only once for a choice.
+// The case of a person voting for multiple choices the most recent is used.
+// the check for the account being allowed to vote is not handled.
 
 // @ts-check
 
@@ -10,6 +10,10 @@ const { assert } = require('console');
 
 const jq = JSON.parse;
 
+/**
+ * @param {string[]} argv
+ * @param {{ fsp: any, http: any, echo: (txt: string) => void }} powers
+ */
 async function main(argv, { fsp, http, echo }) {
   // console.log(argv);
   // TODO: consider docopt if this gets more complex
@@ -18,7 +22,7 @@ async function main(argv, { fsp, http, echo }) {
 
   const ballotData = JSON.parse(await fsp.readFile(ballot, 'utf8'));
 
-  let whichCurl = curl;
+  let whichCurl = url => curl(url, { http });
 
   if (argv.includes('--test')) {
     runTests(ballotData, { fsp });
@@ -66,6 +70,10 @@ const testSuite = [
 ];
 
 // TODO: move this from ./src to ./test
+/**
+ * @typedef {{[refID: string]: { shortDesc: string, docLink?: string, yesAddr: string, noAddr: string }}} QAs
+ * @param {QAs} ballotData
+ */
 async function runTests(ballotData, { fsp }) {
   // TODO: ballot data should be part of test input
   for (const testCase of testSuite) {
@@ -88,6 +96,11 @@ async function runTests(ballotData, { fsp }) {
   }
 }
 
+/**
+ * @param {QAs} ballotData
+ * @param {string} server
+ * @param {{ curl: (url: string) => Promise<string>, echo: (txt: string) => void }} powers
+ */
 async function tally(ballotData, server, { curl, echo }) {
   // console.log('ballot:', ballotData);
 
@@ -97,46 +110,52 @@ async function tally(ballotData, server, { curl, echo }) {
 
   const perItem = {};
 
+  /** @type { (items: string[]) => string[] } */
+  const uniq = items => Array.from(new Set(items).values());
+  /** @type { (as: string[], bs: string[]) => Set<string> } */
+  const intersection = (as, bs) => (bss => new Set(as.filter(x => bss.has(x))))(new Set(bs));
+
   for (const [id, item] of Object.entries(ballotData)) {
     const { shortDesc: desc, yesAddr, noAddr } = item;
     echo(desc);
 
-    const yesVotes = uniq(voteData[id].yes
-                          .map(tx => tx.fromAddr));
+    const yesVotes = uniq(voteData[id].yes.map(tx => tx.fromAddr));
     let yes = yesVotes.length;
-    const noVotes = uniq(voteData[id].no
-                         .map(tx => tx.fromAddr));
+    const noVotes = uniq(voteData[id].no.map(tx => tx.fromAddr));
     let no = noVotes.length;
     perItem[id] = { yes: yes, no: no };
     echo(`  ${yes} yes votes ${yesAddr}`);
     echo(`  ${no} no votes ${noAddr}`);
 
-    const double = Array.from(new Set([...yesVotes].filter(x => new Set(noVotes).has(x))));
+    const double = Array.from(intersection(yesVotes, noVotes));
     if (double.length !== 0) {
       echo(` ALERT: ${double} voted both yes and no.`);
       const doubleVotes = await voterTransactions(double, server, { curl });
       for (const voter of double) {
         for (const acct of doubleVotes[voter].map(tx => tx.toAddr)) {
-          if (acct === yesAddr ) {
-            // echo(`yes found`)
-            no = no - 1;
-            perItem[id].no = no;
-            break;
-          } else if (acct === noAddr) {
-            //  echo no found
-            yes = yes - 1;
-            perItem[id].yes = no;
-            break;
+          if (acct === yesAddr ) { // echo(`yes found`)
+            perItem[id].no = no = no - 1; break;
+          } else if (acct === noAddr) { //  echo no found
+            perItem[id].yes = yes = yes - 1; break;
           }
         }
       }
-      echo(`  ${yes} yes votes ${yesAddr}`);echo(`  ${no} no votes ${noAddr}`);
+      echo(`  ${yes} yes votes ${yesAddr}`);
+      echo(`  ${no} no votes ${noAddr}`);
     }
   }
   return perItem;
 }
 
+/**
+ * @typedef {{ fromAddr: string, toAddr: string }} TX
+ * @param {QAs} ballotData
+ * @param {string} server
+ * @param {{ curl: (url: string) => Promise<string> }} powers
+ * @returns {Promise<{[id: string]: { yes: TX[], no: TX[] }}>}
+ */
 async function voteTransactions(ballotData, server, { curl }) {
+  /** @type { {[id: string]: { yes: TX[], no: TX[] }} } */
   const votes = {};
   for (const [id, item] of Object.entries(ballotData)) {
     const { shortDesc, yesAddr, noAddr } = item;
@@ -149,7 +168,14 @@ async function voteTransactions(ballotData, server, { curl }) {
   return votes;
 }
 
+/**
+ * @param {string[]} fromAddrs
+ * @param {string} server
+ * @param {{ curl: (url: string) => Promise<string> }} powers
+ * @returns { Promise<{[voter: string]: TX[] }>}
+ */
 async function voterTransactions(fromAddrs, server, { curl }) {
+  /** @type { {[voter: string]: TX[] } } */
   const byVoter = {};
   for (const voter of fromAddrs) {
     byVoter[voter] = jq(await curl(`http://${server}/api/transfer/${voter}`));
@@ -157,7 +183,11 @@ async function voterTransactions(fromAddrs, server, { curl }) {
   return byVoter;
 }
 
-
+/**
+ * @param {string} url
+ * @param {{ http: any }} powers
+ * @returns {Promise<string>}
+ */
 function curl(url, { http }) {
   // console.log('get', { url });
   return new Promise((resolve, reject) => {
@@ -174,13 +204,10 @@ function curl(url, { http }) {
   })
 }
 
-function uniq(items) {
-  const s = new Set(items);
-  return Array.from(s.values());
+if (require.main === module) {
+  main(process.argv, {
+    fsp: require('fs').promises,
+    http: require('http'),
+    echo: console.log,
+  }).catch(err => console.error(err));
 }
-
-main(process.argv, {
-  fsp: require('fs').promises,
-  http: require('http'),
-  echo: console.log,
-}).catch(err => console.error(err));
