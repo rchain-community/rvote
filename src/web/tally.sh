addd69112ca2f759152df4fc50001ed7a107e71d
Jim Whitescarver <jimscarver@gmail.com>
2020-09-14 12:16:55 -0400

    allow only registered voters to vote.

--- src/web/tally.sh
+++ src/web/tally.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#usage: ./tally.sh [ballotfile] [transaction-server:port]
+#usage: ./tally.sh [ballotfile] [votersfile] [transaction-server:port]
 # https://github.com/rchain-community/rv2020/issues/35
 # an account is counted only once for a choice.
 # The case of a person voting for multiple choices the most recent is used.
-ballot=${1-ballotexample}
+debug=echo  # set this value of debug last for debug ON
+debug=:     # set this value of debug last for debug OFF
+ballot=${1-ballot}
+voters=${2-voters}
 server=${2-kc-strip.madmode.com:7070}
 shortDescs=$(cat "$ballot"|jq -r '.|.[].shortDesc')
 yesAddrs=$(cat "$ballot"|jq -r '.|.[].yesAddr')
 noAddrs=$(cat "$ballot"|jq -r '.|.[].noAddr')
-lastblock=???????
+lastblock=159959156003900 # 100 time current for now
 for n in $(seq $(echo "$shortDescs"|wc -l)); do
   desc=$(echo "$shortDescs"|sed -n "${n}"p)
   yesAddr=$(echo "$yesAddrs"|sed -n "${n}"p)
@@ -16,14 +19,20 @@ for n in $(seq $(echo "$shortDescs"|wc -l)); do
   echo  "$desc"
   yesVotes=$(curl -s http://"$server"/api/transfer/"$yesAddr"| jq -r '.[].fromAddr'|sort -u)
   yes=$(echo "$yesVotes"|wc -l)
-  echo "$yesVotes" yesVotes
+  for acct in $yesVotes; do
+          if grep -q "$acct" voters; then : ok; else echo $acct not registered; let yes=yes-1;fi
+  done
+  for acct in $noVotes; do
+          if grep -q "$acct" voters; then : ok; else echo $acct not registered; let no=no-1;fi
+  done
+  $debug  "$yesVotes" yesVotes
   noVotes=$(curl -s http://"$server"/api/transfer/"$noAddr"| jq -r '.[].fromAddr'|sort -u)
   no=$(echo "$noVotes"|wc -l)
-  echo "$noVotes" novotes
-  echo  "  $yes yes votes $yesAddr";echo "  $no no votes $noAddr"
+  $debug  "$noVotes" novotes
   double=$(printf "$yesVotes\n$noVotes\n"|sort|uniq -d)
+  printf "$yesVotes\n$noVotes\n" >>/tmp/voters
   if [ "$double" != "" ]; then
-    echo ALERT: "$double" voted both yes and no.
+    $debug  ALERT: "$double" voted both yes and no.
     for voter in $double; do
 #           curl -s http://$server/api/transfer/$voter| jq -r '.|.[].deploy.validAfterBlockNumber'
             # .| {xxx: .[].deploy.validAfterBlockNumber}
@@ -33,6 +42,6 @@ for n in $(seq $(echo "$shortDescs"|wc -l)); do
         fi
       done
     done
-    echo  "  $yes yes votes $yesAddr";echo "  $no no votes $noAddr"
   fi
+  echo  "  $yes yes votes $yesAddr";echo "  $no no votes $noAddr"
 done
