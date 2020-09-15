663d4f8c65c5a2e7372135e6639d533eb37d43c2
Jim Whitescarver <jimscarver@gmail.com>
2020-09-14 21:27:51 -0400

    add timestamp restriction on valid votes.

--- src/web/tally.sh
+++ src/web/tally.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
-#usage: ./tally.sh [ballotfile] [transaction-server:port]
+#usage: ./tally.sh [ballotfile] [votersfile] [transaction-server:port]
 # https://github.com/rchain-community/rv2020/issues/35
 # an account is counted only once for a choice.
 # The case of a person voting for multiple choices the most recent is used.
-ballot=${1-ballotexample}
+debug=echo  # set this value of debug last for debug ON
+debug=:     # set this value of debug last for debug OFF
+ballot=${1-ballot}
+voters=${2-voters}
 server=${2-kc-strip.madmode.com:7070}
 shortDescs=$(cat "$ballot"|jq -r '.|.[].shortDesc')
 yesAddrs=$(cat "$ballot"|jq -r '.|.[].yesAddr')
 noAddrs=$(cat "$ballot"|jq -r '.|.[].noAddr')
-lastblock=???????
+timestamp=159959156003900 # 100 times current for now. blocknumber not easy to get
 for n in $(seq $(echo "$shortDescs"|wc -l)); do
   desc=$(echo "$shortDescs"|sed -n "${n}"p)
   yesAddr=$(echo "$yesAddrs"|sed -n "${n}"p)
   noAddr=$(echo "$noAddrs"|sed -n "${n}"p)
   echo  "$desc"
-  yesVotes=$(curl -s http://"$server"/api/transfer/"$yesAddr"| jq -r '.[].fromAddr'|sort -u)
+  yesVotes=$(curl -s http://"$server"/api/transfer/"$yesAddr"| jq -r ".[] | select(.deploy.timestamp < $timestamp) | .fromAddr"|sort -u)
   yes=$(echo "$yesVotes"|wc -l)
-  noVotes=$(curl -s http://"$server"/api/transfer/"$noAddr"| jq -r '.[].fromAddr'|sort -u)
+  for acct in $yesVotes; do
+          if grep -q "$acct" voters; then : ok; else echo $acct not registered; let yes=yes-1;fi
+  done
+  noVotes=$(curl -s http://"$server"/api/transfer/"$noAddr"| jq -r ".[] | select(.deploy.timestamp < $timestamp) | .fromAddr"|sort -u)
   no=$(echo "$noVotes"|wc -l)
+  for acct in $noVotes; do
+          if grep -q "$acct" voters; then : ok; else echo $acct not registered; let no=no-1;fi
+  done
+  $debug  "$yesVotes" yesVotes
+  $debug  "$noVotes" novotes
   double=$(printf "$yesVotes\n$noVotes\n"|sort|uniq -d)
+  printf "$yesVotes\n$noVotes\n" >>/tmp/voters
   if [ "$double" != "" ]; then
+    $debug  ALERT: "$double" voted both yes and no.
     for voter in $double; do
+#           curl -s http://$server/api/transfer/$voter| jq -r '.|.[].deploy.validAfterBlockNumber'
+            # .| {xxx: .[].deploy.validAfterBlockNumber}
       for acct in $(curl -s http://"$server"/api/transfer/"$voter"| jq -r '.|.[].toAddr'); do
         if [ "$acct" = "$yesAddr" ]; then : echo yes found; let no=no-1; break
         elif [ "$acct" = "$noAddr" ]; then : echo no found; let yes=yes-1; break
@@ -30,3 +45,5 @@ for n in $(seq $(echo "$shortDescs"|wc -l)); do
   fi
   echo  "  $yes yes votes $yesAddr";echo "  $no no votes $noAddr"
 done
+#cat /tmp/voters|sort|uniq>voters #for testing only
+# cat voters |sed '1,$s/^/"/;1,$s/$/",/;$s/,$/\]/;1s/^/\[/' # acct text list to json list
