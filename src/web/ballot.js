ace85b79db821fe815962bd6f917a9aa996edc12
Dan Connolly <dckc@madmode.com>
2020-09-15 23:08:16 -0500

    UI polish with bootstrap based on user testing
      - Sign In button subsumes Pick Address and Get Questions
        - Subordinate REV address as viz hash title
        - shrink vizHash to 40px
      - Nav Bar with RChain Community logo
        - Coop should use their own logo
      - Progress bar (not yet functional)
      - Cite notice of annual meeting rather than board minutes
      - update sample ballot to include candidate statements
      - Subordinate agenda URI under questions
        - Never mind "carefully verify this URI..."
      - Sign and Submit section, button
      - Show Phlolimit as 0.05, not 5000000

--- src/web/ballot.js
+++ src/web/ballot.js
@@ -56,14 +56,12 @@ export function buildUI({ ethereumAddress, getElementById, querySelectorAll, cre
     const theElt = id => check.notNull(getElementById(id));
     const ui = {
         ballotForm: theElt('ballotForm'),
-        pickButton: check.theButton(theElt('pickAccount')),
-        addrField: check.theInput(theElt('REVAddress')),
+        signIn: check.theButton(theElt('signIn')),
         addrViz: theElt('addrViz'),
         questionList: theElt('questionList'),
         response: check.theTextArea(theElt('response')),
         agendaURI: check.theInput(theElt('agendaURI')),
         agendaUriViz: theElt('agendaUriViz'),
-        getQuestions: check.theButton(theElt('getQuestions')),
         submitResponse: theElt('submitResponse'),
         phloLimit: check.theInput(theElt('phloLimit')),
         deployStatus: theElt('deployStatus'),
@@ -76,17 +74,19 @@ export function buildUI({ ethereumAddress, getElementById, querySelectorAll, cre
     const turnOffSubmit = (form) => { form.addEventListener('submit', event => { event.preventDefault(); }) };
     turnOffSubmit(ui.ballotForm);
 
-    ui.pickButton.addEventListener('click', _ => ethereumAddress().then(ethAddr => {
+    ui.signIn.addEventListener('click', _ => ethereumAddress().then(ethAddr => {
         const revAddr = getAddrFromEth(ethAddr);
         state.account = {
             revAddr,
             name: `gov ${revAddr.slice(0, 8) }`,
             ethAddr: ethAddr.replace(/^0x/, ''),
         };
-        showAccount(state.account, ui.addrViz, ui.addrField);
+        showAccount(state.account, ui.addrViz);
+
+        updateQuestions();
     }));
 
-    const pmt = () => ({ account: state.account, phloLimit: parseInt(ui.phloLimit.value) });
+    const pmt = () => ({ account: state.account, phloLimit: 100000000 * parseFloat(ui.phloLimit.value) });
     /** @type { (status: string) => void } */
     const setStatus = status => { ui.deployStatus.textContent = status; };
     function updateQuestions() {
@@ -106,13 +106,7 @@ export function buildUI({ ethereumAddress, getElementById, querySelectorAll, cre
             setStatus(`${err && typeof err === 'object' && err.message ? err.message : err}`);
         });
     }
-    ui.getQuestions.addEventListener('click', _ => {
-        if (!state.account) {
-            alert('choose account first');
-            return;
-        }
-        updateQuestions();
-    });
+
     vizHash(hashCode(ui.agendaURI.value), ui.agendaUriViz);
     ui.agendaURI.addEventListener('change', _ => vizHash(hashCode(ui.agendaURI.value), ui.agendaUriViz));
     ui.submitResponse.addEventListener('click', _ => {
@@ -129,25 +123,24 @@ export function buildUI({ ethereumAddress, getElementById, querySelectorAll, cre
  * Show Account
  * @param { Account } info
  * @param { Element } imgHolder
- * @param { HTMLInputElement } addrField
  *
  * @typedef {{ revAddr: string, ethAddr: string, name: string }} Account
  */
-function showAccount(info, imgHolder, addrField) {
+function showAccount(info, imgHolder) {
     // First remove the '0x' and convert the 8 digit hex number to
     // decimal with i.e. `parseInt('e30a34bc, 16)` to generate a
     // "jazzicon".
     // -- Parker Sep 2018
     //    https://www.reddit.com/r/ethdev/comments/9fwffj/wallet_ui_trick_mock_the_metamask_account_icon_by/
     console.log(info);
-    addrField.value = info.revAddr;
+    imgHolder.setAttribute('title', info.revAddr);
     const seed = parseInt(info.ethAddr.slice(0, 8), 16);
     vizHash(seed, imgHolder);
 }
 
 
 /** @type { (seed: number, holder: Element) => void } */
-function vizHash(seed, holder, size = 60) {
+function vizHash(seed, holder, size = 40) {
     const el = jazzicon(size, seed);
     holder.innerHTML = '';
     holder.appendChild(el);
@@ -245,27 +238,31 @@ function showQuestions(qas, questionList, { createElement }) {
 const testQuestions = {
     "Member Swag": {
         "shortDesc": "The Item of Business I want to propose is to provide all new members with stickers and t-shirts with the RChain logo on it as part of their membership onboarding package.",
-        "docLink": "https://docs.google.com/docs/SWAG.doc",
+        "docLink": "https://gist.github.com/dckc/ca240e5336d0ee3e4f5cf31c4f629a30#member-swag",
         "yesAddr": "11112i8bYVDYcm4MSbY3d1As28uY151xoMS7AyiTvZ2YmNJ8Nw13v9",
         "noAddr": "11112uGayGEi57D44Drq3V4iw5WWyfXbcVvsDangRTE7TaR3J4U4FD"
     },
     "Board: DaD": {
         "shortDesc": "Daffy Duck for Board Member",
+        "docLink": "https://gist.github.com/dckc/ca240e5336d0ee3e4f5cf31c4f629a30#board-dad",
         "yesAddr": "1111TnFUN7eZBWXp3QQACQRRxpcS5uH5Bpf67vikWhA5e3F6ikAmU",
         "noAddr": "11112Cwtg2Bs4WUAYrXhL9xZXXSXr9Gn62Cty39RhUaBnqjrKkqwAZ"
     },
     "Board: DoD": {
         "shortDesc": "Donald Duck for Board Member",
+        "docLink": "https://gist.github.com/dckc/ca240e5336d0ee3e4f5cf31c4f629a30#board-dod",
         "yesAddr": "1111rbdV9Lsw6DyMSq8ySXDacX7pRUxmVGoYho9gGtfZcQYFdAN42",
         "noAddr": "1111JoeZHDYXqyAgo89VaidQnp7W7M9pvdkFUJTqEBU7SHKx6WF2z"
     },
     "Board: WEC": {
         "shortDesc": "Wile E. Coyote for Board Member",
+        "docLink": "https://gist.github.com/dckc/ca240e5336d0ee3e4f5cf31c4f629a30#board-wec",
         "yesAddr": "11112gUFvJR6JBDYJURETaWUBpEDa1EyjgRHFncEfQ4hGECnciPnhw",
         "noAddr": "11112aoa6NLYomYZro566XZVGEXyCDqeqDcp8Pzg81Ckuws6SexC99"
     },
     "Board: RR": {
         "shortDesc": "Road Runner for Board Member",
+        "docLink": "https://gist.github.com/dckc/ca240e5336d0ee3e4f5cf31c4f629a30#board-rr",
         "yesAddr": "1111krbAKSbyGA9vfa7w4K2pKAxZZn6qjaVEduDLWotDZ8HLt2aXR",
         "noAddr": "1111swBFUPVRwR4ugkDBCvrLwPeR1621B1cHQf3cAkNxt3Zad2eac"
     }
