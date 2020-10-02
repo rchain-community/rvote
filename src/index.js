5f12c548e6cd96f577ba22772d343939f4b93374
Dan Connolly <dckc@madmode.com>
2020-10-02 00:10:50 -0500

    index.js: lint

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
+/* global document, window */
 import Shepherd from 'shepherd.js';
 
 import { ethereumAddress } from './vendor/rnode-client-js/src/eth/eth-wrapper.js';
 import { addTour } from './web/tour-help.js';
 import { buildUI } from './web/ballot.js';
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', (_event) => {
   buildUI({
     ethereumAddress,
     fetch: window.fetch,
     now: Date.now,
-    createElement: tag => document.createElement(tag),
-    getElementById: id => document.getElementById(id),
-    querySelectorAll: selector => document.querySelectorAll(selector),
+    createElement: (tag) => document.createElement(tag),
+    getElementById: (id) => document.getElementById(id),
+    querySelectorAll: (selector) => document.querySelectorAll(selector),
   });
 });
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', (_event) => {
   addTour((...args) => new Shepherd.Tour(...args));
 });
