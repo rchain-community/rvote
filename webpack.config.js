94616c258d799c1f8451b0d66a47c0345397d231
Dan Connolly <dckc@madmode.com>
2020-10-01 20:06:02 -0500

    webpack config for HTML, CSS
     - ask webpack for Buffer

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
-/* global require, module, __dirname */
+/* global require, module, __dirname, process */
 
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: process.env.NODE_ENV,
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
+    hot: true,
   },
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  node: {
+    Buffer: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/web/index.html',
+      filename: 'index.html',
+    }),
+  ],
 };
