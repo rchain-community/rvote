b274832ddcfd64a02065cb543eff6afad83854fb
Dan Connolly <dckc@madmode.com>
2020-10-05 21:36:10 -0500

    tally.sql: screen unreigstered voters; fix latest join
      - factor out valid_votes view

--- src/cli/tally.sql
+++ src/cli/tally.sql
@@ -1,6 +1,13 @@
-with vote_cast as (
-    select distinct qid, fromAddr, toAddr, amount, timestamp
+drop view if exists valid_votes;
+create view valid_votes as
+with valid_voter as (
+    select *
     from tx
+    where tx.fromAddr in (select revAddr from voter)
+),
+vote_cast as (
+    select distinct qid, fromAddr, toAddr, choice.prop, amount, timestamp
+    from valid_voter tx
     join choice on choice.addr = tx.toAddr
 ),
 latest as (
@@ -9,14 +16,22 @@ latest as (
     group by qid, fromAddr
 ),
 latest_vote as (
-    select l.qid, l.fromAddr, c.toAddr, l.max_ts
+    select l.qid, l.fromAddr, c.toAddr, c.prop, c.amount, c.timestamp
     from latest l
-    join vote_cast c on l.qid = c.qid and l.fromAddr = c.fromAddr
+    join vote_cast c on l.qid = c.qid and l.fromAddr = c.fromAddr and l.max_ts = c.timestamp
 )
-select choice.qid, replace(prop, 'Addr', '') sentiment, count(distinct v.fromAddr) qty
+select * from latest_vote
+;
+
+drop view if exists tally;
+create view tally as
+select choice.qid, replace(choice.prop, 'Addr', '') sentiment, count(distinct v.fromAddr) qty
 from choice
-join latest_vote v on v.toAddr = choice.addr
-group by choice.qid, prop
+join valid_votes v on v.toAddr = choice.addr
+group by choice.qid, choice.prop
 order by choice.qid, qty desc
 ;
 
+select qid, prop, addr from choice order by qid, prop;
+select * from valid_votes order by fromAddr, qid;
+select * from tally;
