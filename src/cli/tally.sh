dde055d6003e5172a1e7fc471e9eaf7c3cf05bb2
Jim Whitescarver <jimscarver@gmail.com>
2020-10-15 09:47:09 -0400

    added save and replay API calls for regression testing

--- src/cli/tally.sh
+++ src/cli/tally.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #usage: ./tally.sh [ballotfile] [votersfile] [timestamp] [transaction-server:port]
+#usage: save=xxx ./tally.sh [ballotfile] [votersfile] [timestamp] [transaction-server:port]
+#usage: replay=xxx ./tally.sh [ballotfile] [votersfile] [timestamp] [transaction-server:port]
 # https://github.com/rchain-community/rv2020/issues/35
 # an account is counted only once for a choice.
 # The case of a person voting for multiple choices the most recent is used.
@@ -8,7 +10,18 @@ debug=:     # set this value of debug last for debug OFF
 ballot=${1-../web/ballotexample.json}
 voters=${2-voters}
 timestamp=${3-$(date +%s)000} # current timestamp default = seconds since epic times 1000
+if [ "$save" ]; then mkdir saved/"$save"; fi
 server=${4-kc-strip.madmode.com:7070}
+transactions="curl -s http://"$server"/api/transfer"
+trans () {
+  if [ "$save" ]; then
+	$transactions/"$1"|tee saved/"$save"/"$1"
+  elif [ "$replay" ]; then
+	cat saved/"$replay"/"$1"
+  else
+	$transactions/"$1"
+  fi
+}
 shortDescs=$(cat "$ballot"|jq -r '.|.[].shortDesc')
 yesAddrs=$(cat "$ballot"|jq -r '.|.[].yesAddr')
 noAddrs=$(cat "$ballot"|jq -r '.|.[].noAddr')
@@ -19,17 +32,17 @@ for n in $(seq $(echo "$shortDescs"|wc -l)); do
   noAddr=$(echo "$noAddrs"|sed -n "${n}"p)
   abstainAddr=$(echo "$abstainAddrs"|sed -n "${n}"p)
   echo  "$desc"
-  yesVotes=$(curl -s http://"$server"/api/transfer/"$yesAddr"| jq -r ".[] | select(.deploy.timestamp < $timestamp) | .fromAddr"|sort -u)
+  yesVotes=$(trans "$yesAddr"| jq -r ".[] | select(.deploy.timestamp < $timestamp) | .fromAddr"|sort -u)
   yes=$(echo "$yesVotes"|wc -l)
   for acct in $yesVotes; do
           if grep -q "$acct" voters; then : ok; else echo $acct not registered; let yes=yes-1;fi
   done
-  noVotes=$(curl -s http://"$server"/api/transfer/"$noAddr"| jq -r ".[] | select(.deploy.timestamp < $timestamp) | .fromAddr"|sort -u)
+  noVotes=$(trans "$noAddr"| jq -r ".[] | select(.deploy.timestamp < $timestamp) | .fromAddr"|sort -u)
   no=$(echo "$noVotes"|wc -l)
   for acct in $noVotes; do
           if grep -q "$acct" voters; then : ok; else echo $acct not registered; let no=no-1;fi
   done
-  abstainVotes=$(curl -s http://"$server"/api/transfer/"$abstainAddr"| jq -r ".[] | select(.deploy.timestamp < $timestamp) | .fromAddr"|sort -u)
+  abstainVotes=$(trans "$abstainAddr"| jq -r ".[] | select(.deploy.timestamp < $timestamp) | .fromAddr"|sort -u)
   $debug  "$yesVotes" yesVotes
   $debug  "$noVotes" novotes
   $debug  "$abstainVotes" abstainvotes
@@ -39,7 +52,7 @@ for n in $(seq $(echo "$shortDescs"|wc -l)); do
     $debug "  $yes yes votes $yesAddr";$debug "  $no no votes $noAddr"
     $debug  ALERT: "$double" voted both yes and no or abstain.
     for voter in $double; do let found=0
-     for acct in $(curl -s http://"$server"/api/transfer/"$voter"| jq -r '.|.[].toAddr'); do
+     for acct in $(trans "$voter"| jq -r '.|.[].toAddr'); do
         #if [ "$found" == "0" ]; then found=1;: most recent vote remains; else
           if [  "$acct" = "$yesAddr" ]; then $debug yes found
             no=$(grep -v "$voter" <(echo "$noVotes")|wc -l)
@@ -56,7 +69,19 @@ for n in $(seq $(echo "$shortDescs"|wc -l)); do
       done
     done
   fi
-  echo  "  $yes yes votes $yesAddr";echo "  $no no votes $noAddr"
+  result=$(echo  "  $yes yes votes $yesAddr";echo "  $no no votes $noAddr")
+  failed=false
+  if [ "$save" ]; then
+	  echo "$result" > saved/"$save"/result$n
+  elif [ "$replay" ]; then
+	  if [ "$(cat saved/$replay/result$n)" != "$result" ]; then echo ERROR: results do not match for replay "$replay" "$n" >&2
+		  cat saved/$replay/result$n
+		  failed=true
+	  else echo Replay "$replay" matched
+	  fi
+  fi
+  echo "$result"
 done
+if $failed; then echo  FAILED: results do not match for replay "$replay" >&2; fi
 #cat /tmp/voters|sort|uniq>voters #for testing only
 # cat voters |sed '1,$s/^/"/;1,$s/$/",/;$s/,$/\)/;1s/^/Set\(/' # acct text list to json list
